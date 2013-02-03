c7b524976f9bcf4d91969a5f8ad7f3a033f3467f
Ben Wheatley <ben@openanswers.com>
2013-02-03 01:30:49 +0000

    Add *extremely* primitive setup script - until I write something better

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -b -s ~/dotfiles/bashrc ~/.bashrc
+ln -b -s ~/dotfiles/bashrc.functions ~/.bashrc.functions
+ln -b -s ~/dotfiles/inputrc ~/.inputrc
+ln -b -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -b -s ~/dotfiles/vim ~/.vim
+ln -b -s ~/dotfiles/vim/vimrc ~/.vimrc
