f52f7b178c540be48fa70f1b650c19599e30f262
Ben Wheatley <contact@benwh.com>
2013-02-11 22:43:23 +0000

    Add gitconfig

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ ln -b -s ~/dotfiles/inputrc ~/.inputrc
 ln -b -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -b -s ~/dotfiles/vim ~/.vim
 ln -b -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -b -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -b -s ~/dotfiles/gitignore.global ~/.gitignore
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
