25576c9780aafde140cbad2adc1087caa5894924
Ben Wheatley <contact@benwh.com>
2024-08-30 00:50:18 +0100

    nvim: Setup linters

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -5,17 +5,20 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
 
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft = lint.linters_by_ft or {}
+      lint.linters_by_ft['cue'] = { 'cue' }
+      lint.linters_by_ft['gitcommit'] = { 'gitlint', 'vale' }
+      lint.linters_by_ft['go'] = { 'golangcilint' }
+      lint.linters_by_ft['markdown'] = { 'markdownlint-cli2', 'vale' }
+      lint.linters_by_ft['ruby'] = { 'ruby', 'rubocop' }
+      lint.linters_by_ft['rego'] = { 'opa_check' }
+      lint.linters_by_ft['sh'] = { 'shellcheck' }
+      lint.linters_by_ft['sql'] = { 'sqlfluff' }
+      lint.linters_by_ft['yaml'] = { 'yamllint' }
+
       --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
+      -- This effectively also enables a set of default linters:
       -- {
       --   clojure = { "clj-kondo" },
       --   dockerfile = { "hadolint" },
@@ -29,22 +32,10 @@ return {
       --   text = { "vale" }
       -- }
       --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
-
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
         group = lint_augroup,
         callback = function()
           lint.try_lint()
