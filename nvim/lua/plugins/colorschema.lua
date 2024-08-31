174e89e25f5eb9566caab6e50eee5800dbcf35c8
Ben Wheatley <contact@benwh.com>
2024-08-31 01:55:50 +0100

    nvim: clean up unused tokyonight

--- nvim/lua/plugins/colorschema.lua
+++ nvim/lua/plugins/colorschema.lua
@@ -27,6 +27,15 @@ return {
   -- Other colorschemes, rejected for now.
 
   -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- {
   --   'rebelot/kanagawa.nvim',
   --   config = function()
   --     require('kanagawa').setup {
