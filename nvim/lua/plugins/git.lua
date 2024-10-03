6e1e356ced1432ad3a846093a43ad4135fe159f4
Ben Wheatley <contact@benwh.com>
2024-10-03 23:45:43 +0100

    nvim: add rhubarb

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,6 +1,10 @@
 return {
   {
     'tpope/vim-fugitive',
+    dependencies = {
+      -- GitHub support
+      'tpope/vim-rhubarb',
+    },
     config = function()
       vim.keymap.set('n', 'gB', '<cmd>Git blame<CR>', { desc = 'git [B]lame' })
     end,
