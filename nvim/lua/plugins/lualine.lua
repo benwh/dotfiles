cb4db8dd4fbd77a944c151ddb701641d05773d27
Ben Wheatley <contact@benwh.com>
2025-10-31 21:28:58 +0000

    nvim: add lualine status line plugin
    Add nvim-lualine/lualine.nvim for enhanced status line

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      -- tabline = {
+      --   lualine_a = {
+      --     {
+      --       'buffers',
+      --       mode = 4, -- Shows buffer name + buffer number
+      --       max_length = vim.o.columns,
+      --       show_filename_only = false,
+      --       section_separators = { left = '', right = '' },
+      --       component_separators = { left = '', right = '' },
+      --     },
+      --   },
+      -- },
+    },
+  },
+}
