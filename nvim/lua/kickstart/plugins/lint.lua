511ecd9dd7b6c875172005546fe7511b34a45fc0
Ben Wheatley <contact@benwh.com>
2025-10-31 21:41:38 +0000

    nvim: add LintInfo command to lint plugin
    Add LintInfo command to show configured linters for current buffer's
    filetype

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -17,6 +17,28 @@ return {
       lint.linters_by_ft['sql'] = { 'sqlfluff' }
       lint.linters_by_ft['yaml'] = { 'yamllint' }
 
+      local markdown = lint.linters['markdownlint-cli2']
+      markdown.args = {
+        -- There aren't any args by default, so we can just add ours in
+        -- TODO: This doesn't work, for some reason. Needs some debugging.
+        -- Maybe move back to the v1 CLI?
+        -- See markdownlint-cli2/issues/364
+        -- '--config',
+        -- '~/.markdownlint.yaml',
+      }
+
+      -- Show linters for the current buffer's file type
+      vim.api.nvim_create_user_command('LintInfo', function()
+        local filetype = vim.bo.filetype
+        local linters = require('lint').linters_by_ft[filetype]
+
+        if linters then
+          print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+        else
+          print('No linters configured for filetype: ' .. filetype)
+        end
+      end, {})
+
       --
       -- This effectively also enables a set of default linters:
       -- {
