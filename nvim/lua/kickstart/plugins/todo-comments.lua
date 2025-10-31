9e033c806fdefef9a578c610dcd0bb018b8ee617
Ben Wheatley <contact@benwh.com>
2025-10-31 21:41:54 +0000

    nvim: customize todo-comments highlight pattern
    Adjust highlight pattern to better match TODO comments

--- nvim/lua/kickstart/plugins/todo-comments.lua
+++ nvim/lua/kickstart/plugins/todo-comments.lua
@@ -4,7 +4,13 @@ return {
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
+    opts = {
+      signs = false,
+      highlight = {
+        pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+        after = '',
+      },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
