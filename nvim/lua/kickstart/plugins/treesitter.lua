30d591ee009e6e27d09717510fc97333d880ea7a
Ben Wheatley <contact@benwh.com>
2025-10-31 21:41:54 +0000

    nvim: disable treesitter indent for jsonnet and sql
    Disable treesitter-based indentation for jsonnet and sql filetypes

--- nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/lua/kickstart/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby', 'yaml' } },
+      indent = { enable = true, disable = { 'ruby', 'yaml', 'jsonnet', 'sql' } },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -24,6 +24,7 @@ return {
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
       --
+      --    -- TODO: check it out!
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
